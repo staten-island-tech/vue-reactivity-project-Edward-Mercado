0ba7662ffb6e1418c2a1cde75b6a74e0f27a5364
Edward-Mercado <edwardmercado149@gmail.com>
2026-02-06 13:38:23 -0500

    what am i even doing

---
+++ app/src/App.vue
@@ -0,0 +1,14 @@
+<script setup></script>
+
+<template>
+  <h1>You did it!</h1>
+  <p>
+    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
+    documentation
+    <span class="bg-red-100"> hi </span>
+  </p>
+</template>
+
+<style scoped>
+@import 'tailwindcss';
+</style>
