33a4a198fa9d804e87453510222d3b53a3bb0657
Edward-Mercado <edwardmercado149@gmail.com>
2026-02-07 17:56:35 -0500

    working???

--- app/src/views/HomeView.vue
+++ app/src/views/HomeView.vue
@@ -1,18 +1,34 @@
 <template>
-    <section class="w-full h-full p-5 flex flex-col items-center bg-slate-900">
+    <section class="w-full h-full p-5 flex flex-col bg-slate-900">
         <div class="flex lg:flex-row w-full justify-between items-center flex-col gap-4">
-            <h1 class='funnel-sans-title text-8xl lg:text-left text-center bg-gradient-to-l from-white to-cyan-100 inline-block text-transparent bg-clip-text'> Reminder Creator</h1>
-            <div class="flex flex-col flex-nowrap gap-4 lg:w-[20%] sm:w-[80%]">
-                <button class="btn bg-white hover:bg-cyan-200 h-15 px-2 text-black funnel-sans-subttile "> Accounts</button>
-                <button class="btn bg-white hover:bg-cyan-200 h-15 px-2 text-black funnel-sans-subttile "> Delete Reminders... </button>
+            <h1 class='funnel-sans-title text-8xl lg:text-left text-center bg-gradient-to-l from-white to-cyan-200 inline-block text-transparent bg-clip-text'> Reminder Tracker</h1>
+            <div class="flex lg:flex-row flex-col bg-slate-300/70 flex-nowrap gap-4 lg:relative fixed right-2 p-2 rounded-box">
+                <button class="transition duration-300 ease-in-out active:rotate-[5deg] transform btn bg-white hover:bg-cyan-200 active:bg-cyan-400 lg:h-20 lg:w-20 h-15 w-15 px-2 text-black funnel-sans-subtitle p-3"> <img src="/images/user-icon.png" alt="profiles" class="max-h-full"></button>
+                <button class="transition duration-300 ease-in-out active:rotate-[5deg] transform btn bg-white hover:bg-red-300 active:bg-red-500 lg:h-20 lg:w-20 h-15 w-15 px-2 text-black funnel-sans-subttile p-3"> <img src="/images/trashcan.png" alt="delete" class="max-h-full"> </button>
             </div>
         </div>
-        <div class="bg-cyan-200 w-full h-[1%] rounded-full my-3"> </div>
-        <div v-if="creatingReminder" class="fixed left-[5%] top-[5%] w-[90%] h-[90%] bg-slate-300/70 z-10"> 
-            you are creating a reminder. 
-            <button class="btn bg-white text-black hover:bg-purple-500 px-5 z-100" @click="toggleReminder()"> close </button>
+        <div class="bg-cyan-200 w-full h-[1%] rounded-full my-5"> </div>
+
+        <div class="flex flex-row flex-wrap justify-between">
+            <div class="flex-row flex w-[40%] gap-4">
+                <button @click="creatingReminder = true" class="btn bg-white hover:bg-emerald-300 active:bg-emerald-500 px-2 text-black funnel-sans-title p-3 text-2xl h-10 w-10 transition duration-150 ease-in-out active:rotate-[-7deg] transform"> + </button>
+                <h2 class="funnel-sans-title text-3xl"> Reminders... </h2>      
+            </div>
+            <button @click="sortingReminders = !sortingReminders"  class="btn bg-white hover:bg-cyan-200 active:bg-cyan-400 px-2 text-black funnel-sans-subtitle font-black p-3 w-[20%]"> Sort... </button>
         </div>
 
+        <div v-if="sortingReminders" class=" flex flex-col flex-wrap justify-between pt-3 gap-1">
+            <h2 class="funnel-sans-title text-3xl italic"> Sorting Options </h2> 
+
+            <div class="flex flex-row flex-wrap justify-between">
+                <button class="btn bg-white hover:bg-cyan-200 active:bg-cyan-400 px-2 text-black funnel-sans-subtitle font-black p-3 lg:w-[22%] w-[49%] my-2"> Alphabetically </button>
+                <button class="btn bg-white hover:bg-cyan-200 active:bg-cyan-400 px-2 text-black funnel-sans-subtitle font-black p-3 lg:w-[22%] w-[49%] my-2"> Urgency </button>
+                <button class="btn bg-white hover:bg-cyan-200 active:bg-cyan-400 px-2 text-black funnel-sans-subtitle font-black p-3 lg:w-[22%] w-[49%]  my-2"> Date Due </button>
+                <button class="btn bg-white hover:bg-cyan-200 active:bg-cyan-400 px-2 text-black funnel-sans-subtitle font-black p-3 lg:w-[22%] w-[49%]  my-2"> Date Created </button>
+            </div>
+        </div>
+
+        <div class="bg-cyan-200 w-full h-[1%] rounded-full my-5"> </div>
     </section>
 </template>
 
@@ -20,10 +36,7 @@
     import { ref } from 'vue'
 
     let creatingReminder = ref(false)
-    
-    function toggleReminder(){
-        creatingReminder.value = !creatingReminder.value
-    }
+    let sortingReminders = ref(false)
 
 </script>
 
@@ -33,12 +46,10 @@
   font-family: "Funnel Sans", sans-serif;
   font-optical-sizing: auto;
   font-weight: 900;
-  font-style: normal;
 }
 .funnel-sans-subttile {
   font-family: "Funnel Sans", sans-serif;
   font-optical-sizing: auto;
   font-weight: 500;
-  font-style: normal;
 }
 </style>
