6a1f72287efbe8302f71b41c7cd8c10ce2c9bf1b
Edward-Mercado <edwardmercado149@gmail.com>
2026-02-08 02:44:52 -0500

    oh my days

--- app/src/components/UserTitle.vue
+++ app/src/components/UserTitle.vue
@@ -1,7 +1,9 @@
 <template>
     <div class="flex w-full justify-between items-center flex-row">
-        <h1 class='funnel-sans-title text-6xl text-left bg-gradient-to-l from-white to-cyan-200 inline-block text-transparent bg-clip-text w-full'> User Profile Management</h1>
-        <router-link> </router-link>
+        <router-link to="/">
+            <img src="/images/home.png" alt="home" class="h-20 w-20 bg-white rounded-box p-1 hover:bg-cyan-200 active:bg-cyan-400 transition duration-300 ease-in-out active:rotate-[-5deg] transform btn"> 
+        </router-link>
+        <h1 class='funnel-sans-title text-6xl text-right bg-gradient-to-l from-white to-cyan-200 inline-block text-transparent bg-clip-text w-full'> User Profile Management</h1>
     </div>
 </template>
 
